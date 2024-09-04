76aca563c490d8027f355a4ddd1338568295f56d
Radomir Wojtera <radomir.wojtera@gmail.com>
2024-09-04 20:21:36 +0200

    change of approach - native script

--- README.md
+++
@@ -1,2 +0,0 @@
-# time-jumper
-A tool to symulate time jumps (created to chaet time in Fallout Shelpter :-) )
--- commit-msg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Run commitlint on the commit message
-npx --no -- commitlint --edit "$1" || exit 1
-
-# If commitlint passes, run standard-version
-npx standard-version --skip.changelog --skip.tag --skip.bump --skip.commit
